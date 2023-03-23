4f2c598dc528ef24e83e6bdb9948c0a410a34628
Reetus <67768149+reetus-dwai@users.noreply.github.com>
2023-03-23 15:22:19 +0000

    changed join.js to not break if guilds don't have a system channel

--- events/join.js
+++ events/join.js
@@ -8,7 +8,7 @@ module.exports = {
 		size = client.guilds.cache.size;
 		client.user.setActivity(`/help | ${size} servers`, {type: 3});
 		try {
-			channel.send("Thank you for inviting me! Type '/help' for help and '/setup' to get started!");
+			channel?.send("Thank you for inviting me! Type '/help' for help and '/setup' to get started!");
 		} catch (err) {
 			console.log(err);
 		}
